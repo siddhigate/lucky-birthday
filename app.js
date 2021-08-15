3e2daf629c32314c17ec79cb51aee2bacfebaf36
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-08-15 14:54:07 +0530

    feat style: output display

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const input_date = document.querySelector("#date");
 const input_num = document.querySelector("#lucky_num");
 const form = document.querySelector("form");
 
+// output
+const output = document.querySelector(".output_msg");
+
+
 const privacy_close = document.querySelector(".privacy-close");
 const privacy_note = document.querySelector(".privacy-note");
 
@@ -10,10 +14,24 @@ privacy_close.addEventListener("click", function(){
     privacy_note.style.display = "none";
 })
 
+
+function displayResult(isLucky){
+
+    if(isLucky){
+        document.querySelector(".output-img").src = "./lucky.svg";
+        document.querySelector(".output-msg").innerText = "Yayy! Your birthday is lucky :)"; 
+    }
+    else { 
+        document.querySelector(".output-img").src = "./notlucky.svg";
+        document.querySelector(".output-msg").innerText = "Oops! Your birthday is not lucky :("; 
+    }
+}
+
 form.addEventListener('submit', (e)=>{
 
     e.preventDefault();
 
+    console.log("heere");   
     let date_val = (date.value).replaceAll('-',"");
 
     let sum = 0;
@@ -26,6 +44,5 @@ form.addEventListener('submit', (e)=>{
         is_lucky = true;
     }
 
-
-    output_msg.innerHTML = `${input_date.value}    ${input_num.value}   ${date_val}    <b> ${sum}</b> <b> ${is_lucky}</b>`;
+    displayResult(is_lucky)
 })
